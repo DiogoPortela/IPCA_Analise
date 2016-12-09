c477e4b94afb2ee6b6f90c01cc52fa290357e332
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2016-12-09 17:50:52 +0000

    Texto Added

---
+++ Assets/Scripts/GeraNumeros.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeraNumeros : MonoBehaviour {
+    public int numeroDeNumeros;
+    public GameObject text1, text2, text3, text4, text5;
+    int[] numeros;
+
+
+    void Awake ()
+    {
+        Random.InitState((int)Time.time);
+        numeros = new int[numeroDeNumeros]; 
+		for(int i = 0; i < numeroDeNumeros; i++)
+        {
+            numeros[i] = Random.Range(0, 100);
+        }
+        text1.GetComponent<TextMesh>().text = numeros[0].ToString();
+        text2.GetComponent<TextMesh>().text = numeros[1].ToString();
+        text3.GetComponent<TextMesh>().text = numeros[2].ToString();
+        text4.GetComponent<TextMesh>().text = numeros[3].ToString();
+        text5.GetComponent<TextMesh>().text = numeros[4].ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
