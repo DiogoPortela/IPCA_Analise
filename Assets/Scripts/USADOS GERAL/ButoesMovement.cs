ac4a234d6edee41d1a5fc2b7cf18e9f9dee019cc
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2016-12-29 18:40:35 +0000

    New Stuff

---
+++ Assets/Scripts/USADOS GERAL/ButoesMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButoesMovement : MonoBehaviour
+{
+
+    Vector3 position;
+    private void Start()
+    {
+        position = this.gameObject.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("isStanding", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("isStanding", false);
+        }
+    }
+}
