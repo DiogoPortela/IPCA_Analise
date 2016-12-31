ae0a4d1ce471f77b3c417928c08b0bb5a5a2122b
Moisés <liricractos@gmail.com>
2016-12-31 01:53:36 +0000

    tudo bonito e a funcionar

--- Assets/Scripts/Level5function.cs
+++ Assets/Scripts/Level5function.cs
@@ -11,11 +11,18 @@ public class Level5function : MonoBehaviour {
     public int Unidades;
     public int Finalnumber;
     public int RandomCerta;
+    public int wins;
+    public int antiga;
 
     public GameObject Resposta1;
     public GameObject Resposta2;
     public GameObject Resposta3;
     public GameObject Resposta4;
+
+    public GameObject visto1;
+    public GameObject visto2;
+    public GameObject visto3;
+    public GameObject Porta;
 
     //public GameObject Resposta1;
     public GameObject Questao;
@@ -24,45 +31,61 @@ public class Level5function : MonoBehaviour {
     string SCentenas;
     string SDezenas;
     string SUnidades;
-    void Start () {
-            //Cria o número
-        Random.InitState(System.DateTime.Now.Millisecond);
-        DMilhares =(int)Random.Range(2, 9) * 10000;
-        Milhares = (int)Random.Range(1, 9) * 1000;
-        Centenas= (int)Random.Range(1, 9) * 100;
-        Dezenas= (int)Random.Range(2, 9) * 10;
-        Unidades= (int)Random.Range(1, 9);
-        Finalnumber = DMilhares + Milhares + Centenas + Dezenas + Unidades;
 
-        //atribui a certa
+    public void atribuicomeco()
+    {
         RandomCerta = Random.Range(1, 5);
-        if(RandomCerta == 1) { 
+        if (RandomCerta == 1 && antiga != 1)
+        {
             //Resposta1.text = Finalnumber.ToString();
             Resposta1.GetComponent<TextMesh>().text = Finalnumber.ToString();
             Resposta2.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta3.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta4.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
         }
-        else if(RandomCerta == 2) {
+        else if (RandomCerta == 2 && antiga != 2)
+        {
             Resposta1.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta2.GetComponent<TextMesh>().text = Finalnumber.ToString();
             Resposta3.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta4.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
         }
-        else if (RandomCerta == 3)
+        else if (RandomCerta == 3 && antiga != 3)
         {
             Resposta1.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta2.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
             Resposta3.GetComponent<TextMesh>().text = Finalnumber.ToString();
             Resposta4.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
         }
-        else
-        {
-            Resposta1.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
-            Resposta2.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
-            Resposta3.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
-            Resposta4.GetComponent<TextMesh>().text = Finalnumber.ToString();
-        }
+        else if (RandomCerta == 4 || RandomCerta == 5) { 
+            if (antiga != 4 || antiga != 5)
+            {
+                atribuicomeco();
+            }
+            else
+            
+                {
+                    Resposta1.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
+                    Resposta2.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
+                    Resposta3.GetComponent<TextMesh>().text = Random.Range(10000, 99999).ToString();
+                    Resposta4.GetComponent<TextMesh>().text = Finalnumber.ToString();
+                }
+            }
+        else { atribuicomeco(); }
+    }
+    public void comeco()
+    {
+        //Cria o número
+        Random.InitState(System.DateTime.Now.Millisecond);
+        DMilhares = (int)Random.Range(2, 9) * 10000;
+        Milhares = (int)Random.Range(1, 9) * 1000;
+        Centenas = (int)Random.Range(1, 9) * 100;
+        Dezenas = (int)Random.Range(2, 9) * 10;
+        Unidades = (int)Random.Range(1, 9);
+        Finalnumber = DMilhares + Milhares + Centenas + Dezenas + Unidades;
+
+        //atribui a certa
+        atribuicomeco();
 
         switch (DMilhares)
         {
@@ -224,11 +247,72 @@ public class Level5function : MonoBehaviour {
                 break;
         }
 
-        Questao.GetComponent<TextMesh>().text = SDMilhares+SMilhares+SCentenas+SDezenas+SUnidades;
+        Questao.GetComponent<TextMesh>().text = SDMilhares + SMilhares + SCentenas + SDezenas + SUnidades;    
+   }
+    void Start() {
+        wins = 0;
+        comeco();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-}
+        if (wins == 1)
+        {
+            visto1.GetComponent<Renderer>().enabled = true;
+        }
+        else if (wins == 2)
+        {
+            visto1.GetComponent<Renderer>().enabled = true;
+            visto2.GetComponent<Renderer>().enabled = true;
+        }
+        else if (wins == 3)
+        {
+            visto1.GetComponent<Renderer>().enabled = true;
+            visto2.GetComponent<Renderer>().enabled = true;
+            visto3.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            if (wins > 3) wins = 3;
+            else wins = 0;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Resposta1" && RandomCerta == 1)
+        {
+            wins++;
+            antiga = 1;
+            comeco();
+        }
+        else if (other.gameObject.tag == "Resposta2" && RandomCerta == 2)
+        {
+            wins++;
+            antiga = 2;
+            comeco();
+        }
+        else if (other.gameObject.tag == "Resposta3" && RandomCerta == 3)
+        {
+            wins++;
+            antiga = 3;
+            comeco();
+        }
+        else if ((other.gameObject.tag == "Resposta4" && RandomCerta == 4) || (other.gameObject.tag == "Resposta4" && RandomCerta == 5))
+        {
+            wins++;
+            antiga = 4;
+            comeco();
+        }
+
+        if (wins == 3)
+        {
+            Resposta1.GetComponent<Renderer>().enabled = false; 
+            Resposta2.GetComponent<Renderer>().enabled = false; 
+            Resposta3.GetComponent<Renderer>().enabled = false; 
+            Resposta4.GetComponent<Renderer>().enabled = false; 
+            Questao.GetComponent<Renderer>().enabled = false;
+            Porta.GetComponent<MudarNivel>().Vitoria = true;
+        }
+
+    }
+    }
