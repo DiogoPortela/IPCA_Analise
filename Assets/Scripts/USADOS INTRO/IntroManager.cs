c75fad0ea63006233d64520ee82e28671f37666c
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-01-09 22:48:33 +0000

    intro

---
+++ Assets/Scripts/USADOS INTRO/IntroManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    public string[] text;
+    public TextMesh target;
+
+    int counter = 0;
+
+    public void next()
+    {
+        counter++;
+        if(counter < text.Length + 1)
+        {
+            target.text += "\n" + text[counter - 1];
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
